d8aa1e8db18f4c4e3742b5389d6ab9574e13eb68
mmiinnjeong <128569729+mmiinnjeong@users.noreply.github.com>
2023-05-16 06:25:13 +0000

    switch문 추가

---
+++ .ccls-cache/@home@runner@c/mission_myscore.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("성적을 입력하세요.\n");
+//   scanf("%d", &x);
+//   if(x>=90)
+//     printf("A!");
+//   else if(x>=80)
+//     printf("B!");
+//   else if (x>=70)
+//     printf("C!");
+//   else if (x>=60)
+//     printf ("D!");
+//   else
+//     printf("F");
+//   printf("\n다음 학기에도 열심히 합시다!");
+// }
