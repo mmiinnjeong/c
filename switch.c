a0fcc27d82f4a7d825cd8d3070af5de4b95eef9e
mmiinnjeong <128569729+mmiinnjeong@users.noreply.github.com>
2023-05-23 06:11:42 +0000

    while1.c while2.c 추가

--- switch.c
+++ switch.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-main(){
-  int x;
+// #include <stdio.h>
+// main(){
+//   int x;
   
-  printf("당신이 좋아하는 과일은 무엇인가요? 아래의 번호를 선택해주세요.\n");
-  printf("1번:사과, 2번:키위, 3번:복숭아 4번:수박, 0번:모두 좋아하지 않는다\n");
-  scanf("%d", &x);
+//   printf("당신이 좋아하는 과일은 무엇인가요? 아래의 번호를 선택해주세요.\n");
+//   printf("1번:사과, 2번:키위, 3번:복숭아 4번:수박, 0번:모두 좋아하지 않는다\n");
+//   scanf("%d", &x);
 
-  switch(x){
-  case 1:
-    printf("사과를 좋아하는군요!");
-    break;
-  case 2:
-    printf("키위를 좋아하는군요!");
-    break;
-  case 3:
-    printf("복숭아를 좋아하는군요!");
-    break;
-  case 4:
-    printf ("수박을 좋아하는군요!");
-    break;
-  default:
-    printf("당신은 아마도 과일을 좋아하지 않는 것 같군요!");
-  printf("\n좋아하는 과일 조사 끝!");
-}
-}
+//   switch(x){
+//   case 1:
+//     printf("사과를 좋아하는군요!");
+//     break;
+//   case 2:
+//     printf("키위를 좋아하는군요!");
+//     break;
+//   case 3:
+//     printf("복숭아를 좋아하는군요!");
+//     break;
+//   case 4:
+//     printf ("수박을 좋아하는군요!");
+//     break;
+//   default:
+//     printf("당신은 아마도 과일을 좋아하지 않는 것 같군요!");
+//   printf("\n좋아하는 과일 조사 끝!");
+// }
+// }
